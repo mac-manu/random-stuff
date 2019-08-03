5965cd1bc4a9b5c042a798dcc585fb07509e1c7b
macmanu <manu@mac-manu.com>
2019-08-03 23:30:58 +0200

    random

--- main_bundle.rb
+++ main_bundle.rb
@@ -8,7 +8,7 @@ end
 my_password = BCrypt::Password.create("my password")
 
 puts my_password
-puts my_password.version
-puts my_password.cost
+#puts my_password.version
+#puts my_password.cost
 puts my_password == "my password"
 puts my_password == "not my password"
