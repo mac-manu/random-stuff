7d76728f952812bed32b174162b0b1c111a4247b
macmanu <manu@mac-manu.com>
2019-08-12 22:00:02 +0200

    hash2

---
+++ hash2.rb
@@ -0,0 +1,10 @@
+users = [
+          { username: "manux", password: "password1" },
+          { username: "jack", password: "password2" },
+          { username: "arya", password: "password3" },
+          { username: "jonshow", password: "password4" },
+          { username: "heisenberg", password: "password5" }
+        ]
+
+
+puts users[3]
