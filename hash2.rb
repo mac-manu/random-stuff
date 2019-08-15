1f645b21681e8a0cbdea1b2461b941e63a94dbec
macmanu <manu@mac-manu.com>
2019-08-15 12:04:46 +0200

    test2

--- hash2.rb
+++ hash2.rb
@@ -17,3 +17,38 @@ puts users.find {|x| x[:username] == "arya"}
 books         = {}
 books[:matz]  = "The Ruby Programming Language"
 books[:black] = "The Well-Grounded Rubyist"
+
+
+class Book
+  attr_reader :author, :title
+
+  def initialize(author, title)
+    @author = author
+    @title = title
+  end
+
+  def ==(other)
+    self.class === other and
+      other.author == @author and
+      other.title == @title
+  end
+
+  alias eql? ==
+
+  def hash
+    @author.hash ^ @title.hash # XOR
+  end
+end
+
+book1 = Book.new 'matz', 'Ruby in a Nutshell'
+book2 = Book.new 'matz', 'Ruby in a Nutshell'
+
+reviews = {}
+
+reviews[book1] = 'Great reference!'
+reviews[book2] = 'Nice and compact!'
+
+reviews.length #=> 1
+
+puts book1
+puts book2 
