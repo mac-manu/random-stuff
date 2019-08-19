6b453eb7cea2e9f1b842116df1dfb7811963b772
macmanu <manu@mac-manu.com>
2019-08-19 19:00:19 +0200

    test app

---
+++ rails/test_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :articles
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'welcome/home',  to: 'welcome#home'
+  get 'welcome/about',  to: 'welcome#about'
+
+  root 'welcome#home'
+end
