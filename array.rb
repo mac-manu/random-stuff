f8840ea9f17170184958a2f3ded10acc6afc8449
macmanu <manu@mac-manu.com>
2019-09-03 11:29:48 +0200

    arrary struff

---
+++ array.rb
@@ -0,0 +1,12 @@
+puts array = []
+puts array.empty?
+
+puts user = { 'first_name' => 'Bob', 'last_name' => 'Carlton' }
+
+
+user.has_key?('first_name')
+
+book_title = "example book title"
+puts book_title.capitalize!
+
+puts book_title
