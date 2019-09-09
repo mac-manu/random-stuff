94ed2b3c4bf766a7ae204f4fd7df4f66c6998108
macmanu <manu@mac-manu.com>
2019-09-09 22:39:54 +0200

    arrays

--- array.rb
+++ array.rb
@@ -10,3 +10,17 @@ book_title = "example book title"
 puts book_title.capitalize!
 
 puts book_title
+
+
+puts programming_languages = ['Ruby', 'Python', 'Golang', 'C++', 'C', 'D']
+
+puts programming_languages.values_at(0, 2, 4)
+
+puts fruits = { 'orange' => '$2.00', 'apple' => '$3.00', 'grapes' => '$2.50' }
+
+puts fruits.values_at('orange')
+
+
+
+numbers = [1,2,3,45,6,1,2,3,4,9]
+puts numbers.count 
